1e995a7d51fcd038e693496913d66c1ad8609f14
semuaAda <115983651+semuaAda@users.noreply.github.com>
2022-11-04 15:53:12 +0700

    code version 3

--- dasboard.php
+++ dasboard.php
@@ -1,16 +1,35 @@
-<?php 
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dasboard</title>
+    <title>Halaman Utama</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div>Halaman Utama</div>
+    <nav class="navbar">
+        <label class="logo">MA AS-SALAM BANGKALAN</label>
+    <ul class="kotak-keluar">
+        <li><a href="#">View Site</a></li>
+        <li><a href="#">Log Out</a></li>
+    </ul>
+    </nav>
+    <nav class="sidebar">
+        <ul class="kotak-samping">
+            <div>
+            <img src="/img/dita.jpg" alt="image"></li>
+            <h3>Aditania</h3>
+            <h3>200631100126</h3>
+            </div>
+            <li><a href="#">Halaman Depan</a></li>
+            <li><a href="#">Pengumuman</a></li>
+            <li><a href="#">Pembayaran</a></li>
+        </ul>
+    </nav>
+    <div class="content">
+        <h2>Halaman Utama</h2>
+        <p>Selamat Datang username di Portal Akademik, Portal Akademik adalah sistem yang memungkinkan civitas akadamika MA AS-SALAM BANGKALAN untuk menerima informasi dengan lebih cepat malalui internet. Sistem ini diharapkan dapat memberikan kemudahan setiap civitas akademika untuk melakukan aktivitas-aktivitas akademik dan proses belajar mengajar. Slemat mengunakan fasilitas ini.</p>
+    </div>
 </body>
 </html>
